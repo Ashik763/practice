403dcbbbbcff64f0df148b8c16d20bc2289243ad
Ashik763 <ashikghosh763@gmail.com>
2021-02-07 03:11:19 +0600

    first commit

---
+++ callApply.js
@@ -0,0 +1,18 @@
+
+const normalPerson = {
+    firstName: 'Rahim',
+    lastName: 'Uddin',
+    salary:1500,
+    getFullName:function(){     //when a function is declared in a object as a property then the function is called method object
+        console.log(this.firstName,this.lastName);
+    },
+    chargeBill:function(amount){
+        this.salary = this.salary-amount;
+        return this.salary;
+    }
+}
+
+normalPerson.chargeBill(150);
+console.log(normalPerson.salary);
+
+
